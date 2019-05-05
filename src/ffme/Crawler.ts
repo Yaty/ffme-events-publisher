80ecee9f81c9e1c012139ac6dadcf96961e9c8dc
Hugo Da Roit <contact@hdaroit.fr>
2019-05-05 17:11:23 +0200

    Remove useless Promise.all

--- src/ffme/Crawler.ts
+++ src/ffme/Crawler.ts
@@ -28,27 +28,19 @@ export default class Crawler {
   private async login(): Promise<void> {
     await this.page.type('#UTI_LOGIN', this.username);
     await this.page.type('#UTI_MOTPASSE', this.password);
-
-    await Promise.all([
-      this.page.click('.bouton_right'),
-      this.page.waitForSelector('#ID_PROFIL'),
-    ]);
+    await this.page.click('.bouton_right');
+    await this.page.waitForSelector('#ID_PROFIL');
   }
 
   private async selectProfile(): Promise<void> {
     await this.page.click('#ID_PROFIL > option:nth-child(2)');
-
-    await Promise.all([
-      this.page.click('.bouton_right'),
-      this.page.waitForSelector('#menu31'),
-    ]);
+    await this.page.click('.bouton_right');
+    await this.page.waitForSelector('#menu31');
   }
 
   private async selectLigueManagement(): Promise<void> {
-    await Promise.all([
-      this.page.click('#menu31 > p:nth-child(1) > a:nth-child(5)'),
-      this.page.waitForSelector('#cadreTableauOngletGU'),
-    ]);
+    await this.page.click('#menu31 > p:nth-child(1) > a:nth-child(5)');
+    await this.page.waitForSelector('#cadreTableauOngletGU');
   }
 
   private getHTML(): Promise<string> {
